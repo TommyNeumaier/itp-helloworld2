1394fc9fce0a32f7ee4c65d9a4857605f6028dfa
“TommyNeumaier” <t.neumaier@students.htl-leonding.ac.at>
2023-09-15 09:40:14 +0200

    better hello world

--- ij_helloworld/src/main/java/at/htlleonding/Main.java
+++ ij_helloworld/src/main/java/at/htlleonding/Main.java
@@ -2,8 +2,14 @@ package at.htlleonding;
 
 public class Main {
     public static void main(String[] args) {
-        for(int i = 0; i < 5; i++){
-            System.out.println("Hello World!");
+        while(true) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.exit(-1);
+            }
         }
+
+        // System.out.println("Hello World!");
     }
 }
